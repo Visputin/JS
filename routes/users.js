f9f25da269ff4b380b494b83a9ebd11691cfc806
Saska <lasaroffs@gmail.com>
2024-02-18 15:08:48 +0200

    GET ja POST-kyselyt

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,24 @@ router.get('/', function(req, res, next) {
   res.send('Heipähei');
 });
 
+router.get('/esim1', function(request,response) {
+        response.send('tässä esimerkki get-metodista');
+    }
+);
+
+router.get('/esim2/:name', function(request,response) {
+  response.send('Terve ' +request.params.name +(' (tässä get-metodi parametrilla)'));
+  }
+);
+
+router.get('/esim3/:etunimi/:sukunimi', function(request,response) {
+  response.send('Terve ' +request.params.etunimi +" " +request.params.sukunimi +(' (tässä get-metodi kahdella parametrilla)'));
+  }
+);
+
+router.post('/', function(request,response) {
+  response.send(request.body.fname +' ' +request.body.lname)
+  }
+);
+
 module.exports = router;
